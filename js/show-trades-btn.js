f25a08614aaa97acc4695f032a2a7badd463ff4e
limelime <limelime@users.noreply.github.com>
2016-03-09 14:16:51 -0500

    Rename variable to share-price.

--- js/show-trades-btn.js
+++ js/show-trades-btn.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     remove_all_child_elements("trade-details"); // Clear trade-details table first.
     
     var total_amount = $("#total-amount").val();
-    var price_per_share = $("#price-per-share").val();
+    var share_price = $("#share-price").val();
     var commission_fee = $("#commission-fee").val();
     var trade_proportions = $("#trade-proportions").val().split(',');
     var number_of_trades = trade_proportions.length;
@@ -24,12 +24,12 @@ $(document).ready(function(){
     var show_trades_html='<table><tr><th></th><th>Shares</th><th>Price</th><th>Cost</th></tr>'
     for(var i=0; i < number_of_trades; i++)
     {
-      var shares = Math.floor(trade_proportions[i]*portion_amount/price_per_share);
-      var cost = (shares*price_per_share).toFixed(2);
+      var shares = Math.floor(trade_proportions[i]*portion_amount/share_price);
+      var cost = (shares*share_price).toFixed(2);
       show_trades_html+='<tr>';
       show_trades_html+='<td>'+trade_proportions[i]+'</td>';
       show_trades_html+='<td>'+get_input_html(shares, 5)+'</td>';
-      show_trades_html+='<td>'+get_input_html(price_per_share, 5)+'</td>';
+      show_trades_html+='<td>'+get_input_html(share_price, 5)+'</td>';
       show_trades_html+='<td>'+cost+'</td>';
       show_trades_html+='</tr>';
     }
