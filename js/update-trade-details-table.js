a642851bf0b28a76755d14fef769e8f02b3c5cc0
limelime <limelime@users.noreply.github.com>
2016-03-09 16:46:14 -0500

    On change in trade detail table, update cost.

--- js/update-trade-details-table.js
+++ js/update-trade-details-table.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 
+  // On change, recalculate cost.
 	$('#trade-details').on('change', 'input', function(){
-    console.log("helo2");
-    
-    $("#trade-details > table > tbody > tr").each(function() {
-      console.log("ter");
+	
+    $("#trade-details > table > tbody > tr:not(:first-child)").each(function() {
+      var share = $(this).find("td:nth-child(2) > input").val();
+      var share_price = $(this).find("td:nth-child(3) > input").val();
+      var cost = share * share_price;
+      
+      cost = cost.toFixed(2);
+      $(this).find("td:nth-child(4)").text(cost);
     });
     
             
