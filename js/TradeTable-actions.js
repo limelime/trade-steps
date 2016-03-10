f21c41287a1b092fc38625c4fc716007c8289067
limelime <limelime@users.noreply.github.com>
2016-03-10 13:13:00 -0500

    Refactor actions.

---
+++ js/TradeTable-actions.js
@@ -0,0 +1,15 @@
+/**
+ * Trade table actions: ADD, REMOVE.
+ **/
+ 
+ 
+$(document).ready(function(){
+
+  // REMOVE
+  $('#trade-details').on('click', 'input[name="remove-trade"]', function(e){
+     $(this).closest('tr').remove();
+  })
+  
+  // ADD
+  
+});
