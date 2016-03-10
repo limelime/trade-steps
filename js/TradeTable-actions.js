19252c971b6b60efc90c2599adf8f80233127148
limelime <limelime@users.noreply.github.com>
2016-03-10 16:46:05 -0500

    Implemented ADD action.

--- js/TradeTable-actions.js
+++ js/TradeTable-actions.js
@@ -8,16 +8,22 @@
 $(document).ready(function(){
 
   // REMOVE
+  // ***************
   $('#trade-details').on('click', 'input[name="remove-trade"]', function(e){
-     $(this).closest('tr').remove();
+    $(this).closest('tr').remove();
   })
   
   // ADD
+  // ***************
   $('#trade-details').on('click', 'input[name="add-trade"]', function(e){
-     $(this).closest('tr').remove();
+    
+    var tradeTable = new TradeTable();
+    var html_row = tradeTable.buildHtmlRow('?', 0, 0, 0);
+    $(this).closest('tr').after(html_row);
   })
     
   // ON CHANGE, recalculate cost.
+  // ***************
   $('#trade-details').on('change', 'input', function(){
   
     $("#trade-details > table > tbody > tr:not(:first-child)").each(function() {
