380e4b32def900471181d1a099b8bbc892352d48
limelime <limelime@users.noreply.github.com>
2016-03-10 16:34:22 -0500

    Refactor to group all actions together.

--- js/TradeTable-actions.js
+++ js/TradeTable-actions.js
@@ -1,5 +1,7 @@
 /**
- * Trade table actions: ADD, REMOVE.
+ * Trade table actions:
+ *    -ADD, REMOVE buttons
+ *    -ON CHANGE, update cost.
  **/
  
  
@@ -15,4 +17,18 @@ $(document).ready(function(){
      $(this).closest('tr').remove();
   })
     
+  // ON CHANGE, recalculate cost.
+  $('#trade-details').on('change', 'input', function(){
+  
+    $("#trade-details > table > tbody > tr:not(:first-child)").each(function() {
+      var share = $(this).find("td:nth-child(2) > input").val();
+      var share_price = $(this).find("td:nth-child(3) > input").val();
+      var cost = share * share_price;
+      
+      cost = cost.toFixed(2);
+      $(this).find("td:nth-child(4)").text(cost);
+    });
+    
+  });
+  
 });
