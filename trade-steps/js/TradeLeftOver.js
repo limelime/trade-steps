f18e93bf925b0b5b5ab329789cd8b6bbde54d597
limelime <limelime@users.noreply.github.com>
2018-09-14 23:06:58 +0000

    Fix #1: Rename Left over to Balance.

--- trade-steps/js/TradeLeftOver.js
+++ trade-steps/js/TradeLeftOver.js
@@ -13,13 +13,13 @@ TradeLeftOver.update = function (total_cost, total_fee, total_share)
   // Get numbers.
   var tradeInfo = new TradeInfo();
   var total_amount = tradeInfo.getAmount();
-  var remainder = total_amount - total_cost - total_fee;
+  var balance = total_amount - total_cost - total_fee;
   var average_price = (total_cost + total_fee) / total_share;
   
   // Reformat numbers and append.
   total_amount= parseFloat(total_amount).toFixed(2);
   total_cost  = parseFloat(total_cost).toFixed(2);
-  remainder   = parseFloat(remainder).toFixed(2);
+  balance   = parseFloat(balance).toFixed(2);
   average_price = average_price.toFixed(4);
   
   // Create table holding left over infos.
@@ -30,14 +30,14 @@ TradeLeftOver.update = function (total_cost, total_fee, total_share)
 	                          '<th>Total amount</th><th>-</th>'+
 	                          '<th>Total cost</th><th>-</th>'+
 	                          '<th>Commission fee</th><th>=</th>'+
-	                          '<th>Left over</th>'+
+	                          '<th>Balance</th>'+
                           '</tr>';
                           
       left_over_html += '<tr>'+
                             '<td class="number-2">'+total_amount+'</td><td>-</td>'+
                             '<td class="number-2">'+total_cost+'</td><td>-</td>'+
                             '<td class="number-2">'+total_fee+'</td><td>=</td>'+
-                            '<td class="number-2">'+remainder+'</td>'+
+                            '<td class="number-2">'+balance+'</td>'+
                           '</tr>';
                                           
       left_over_html += '</table>';
