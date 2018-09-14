1a9fdb9b60a0d183a034970765c67a9d8c3c2a59
limelime <limelime@users.noreply.github.com>
2018-09-14 23:57:59 +0000

    rename variable.

--- trade-steps/js/TradeBalance.js
+++ trade-steps/js/TradeBalance.js
@@ -22,30 +22,30 @@ TradeBalance.update = function (total_cost, total_fee, total_share)
 	balance     = parseFloat(balance).toFixed(2);
 	average_price = average_price.toFixed(4);
   
-	// Create table holding balance infos.
-	var balance_html = '<br />';
-		balance_html += '<table><tr>'+
+	// Create table holding summary infos.
+	var summary_html = '<br />';
+		summary_html += '<table><tr>'+
 									'<th>Total amount</th><th>-</th>'+
 									'<th>Total cost</th><th>-</th>'+
 									'<th>Commission fee</th><th>=</th>'+
 									'<th>Balance</th>'+
 								'</tr>';
 												
-		balance_html += '<tr>'+
+		summary_html += '<tr>'+
 							'<td class="number-2">'+total_amount+'</td><td>-</td>'+
 							'<td class="number-2">'+total_cost+'</td><td>-</td>'+
 							'<td class="number-2">'+total_fee+'</td><td>=</td>'+
 							'<td class="number-2">'+balance+'</td>'+
 						'</tr>';
 
-		balance_html += '</table>';
+		summary_html += '</table>';
 
 		// Share and average price
-		balance_html += '<br /><span>Total shares: </span><span class="number-0">'+total_share+'</span><br />';
-		balance_html += '<span>Average price: </span><span class="number-5">'+average_price+'</span>';
+		summary_html += '<br /><span>Total shares: </span><span class="number-0">'+total_share+'</span><br />';
+		summary_html += '<span>Average price: </span><span class="number-5">'+average_price+'</span>';
   
 		// Append
-		$('#trade-balance').append(balance_html);
+		$('#trade-balance').append(summary_html);
 
 	// Trigger number formatting using jquery-number.
 	$('.number-0').number( true, 0 );
