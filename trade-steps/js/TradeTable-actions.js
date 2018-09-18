209ab60f75d0d02ca4301fad104a5b65ff31e7db
limelime <limelime@users.noreply.github.com>
2018-09-18 18:09:38 +0000

    Fix #4: On Remove, update Balance.

--- trade-steps/js/TradeTable-actions.js
+++ trade-steps/js/TradeTable-actions.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
   // ***************
   $('#trade-details').on('click', 'input[name="remove-trade"]', function(e){
     $(this).closest('tr').remove();
+    TradeSummary.updateOnChange();
   })
   
   // ADD
   // ***************
   $('#trade-details').on('click', 'input[name="add-trade"]', function(e){
-    
     var tradeTable = new TradeTable();
     var html_row = tradeTable.buildHtmlRow('?', 0, 0, 0);
     $(this).closest('tr').after(html_row);
@@ -25,7 +25,6 @@ $(document).ready(function(){
   // ON CHANGE, Update cost.
   // ***************
   $('#trade-details').on('change', 'input', function(){
-	
 	TradeSummary.updateOnChange();
   });
   
