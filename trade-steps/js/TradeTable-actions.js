c7a5057faa6fb4856944740c877a6ac6f2298f91
limelime <limelime@users.noreply.github.com>
2018-09-18 19:02:17 +0000

    indent.

--- trade-steps/js/TradeTable-actions.js
+++ trade-steps/js/TradeTable-actions.js
@@ -7,25 +7,25 @@
  
 $(document).ready(function(){
 
-  // REMOVE
-  // ***************
-  $('#trade-details').on('click', 'input[name="remove-trade"]', function(e){
-    $(this).closest('tr').remove();
-    TradeSummary.updateOnChange();
-  })
+	// REMOVE
+	// ***************
+	$('#trade-details').on('click', 'input[name="remove-trade"]', function(e){
+		$(this).closest('tr').remove();
+		TradeSummary.updateOnChange();
+	})
   
-  // ADD
-  // ***************
-  $('#trade-details').on('click', 'input[name="add-trade"]', function(e){
-    var tradeTable = new TradeTable();
-    var html_row = tradeTable.buildHtmlRow('?', 0, 0, 0);
-    $(this).closest('tr').after(html_row);
-  })
+	// ADD
+	// ***************
+	$('#trade-details').on('click', 'input[name="add-trade"]', function(e){
+		var tradeTable = new TradeTable();
+		var html_row = tradeTable.buildHtmlRow('?', 0, 0, 0);
+		$(this).closest('tr').after(html_row);
+	})
     
-  // ON CHANGE, Update cost.
-  // ***************
-  $('#trade-details').on('change', 'input', function(){
-	TradeSummary.updateOnChange();
-  });
+	// ON CHANGE, Update cost.
+	// ***************
+	$('#trade-details').on('change', 'input', function(){
+		TradeSummary.updateOnChange();
+	});
   
 });
