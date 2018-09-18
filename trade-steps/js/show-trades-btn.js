746f5739fbd1a473780e3fc9c758b63454fe2854
limelime <limelime@users.noreply.github.com>
2018-09-18 21:38:55 +0000

    Fix #8: Remove redundant onClick.

--- trade-steps/js/show-trades-btn.js
+++ trade-steps/js/show-trades-btn.js
@@ -26,44 +26,40 @@ $(document).ready(function(){
 			e.preventDefault();
 			
 			// Show trades.
-		//	$("#show-trades-btn").click(function() {
+			remove_all_child_elements("trade-details"); // Clear trade-details table first.
+			
+			var tradeInput = new TradeInput();
+			
+			var total_amount = tradeInput.getAmount();
+			var share_price = tradeInput.getSharePrice();
+			var commission_fee = tradeInput.getCommissionFee();
+			var trade_portions = tradeInput.getPortions();
+			var number_of_trades = trade_portions.length;
+			
+			// Get total proportion.
+			var total_proportion = 0;
+			for(var i=0; i < number_of_trades; i++)
+			{
+				total_proportion += parseInt(trade_portions[i]);
+			}
+			
+			// Calculate 1 portion amount.
+			var portion_amount = (total_amount-(number_of_trades*commission_fee)) / total_proportion; // Note: Subtract commission fees.
+			
+			// Display trades result.
+			var tradeTable = new TradeTable("#trade-details");
+			tradeTable.create();
+			for(var i=0; i < number_of_trades; i++)
+			{
+				var shares = Math.floor(trade_portions[i]*portion_amount/share_price);
+				var cost = (shares*share_price).toFixed(2);
 				
-				remove_all_child_elements("trade-details"); // Clear trade-details table first.
-				
-				var tradeInput = new TradeInput();
-				
-				var total_amount = tradeInput.getAmount();
-				var share_price = tradeInput.getSharePrice();
-				var commission_fee = tradeInput.getCommissionFee();
-				var trade_portions = tradeInput.getPortions();
-				var number_of_trades = trade_portions.length;
-				
-				// Get total proportion.
-				var total_proportion = 0;
-				for(var i=0; i < number_of_trades; i++)
-				{
-					total_proportion += parseInt(trade_portions[i]);
-				}
-				
-				// Calculate 1 portion amount.
-				var portion_amount = (total_amount-(number_of_trades*commission_fee)) / total_proportion; // Note: Subtract commission fees.
-				
-				// Display trades result.
-				var tradeTable = new TradeTable("#trade-details");
-				tradeTable.create();
-				for(var i=0; i < number_of_trades; i++)
-				{
-					var shares = Math.floor(trade_portions[i]*portion_amount/share_price);
-					var cost = (shares*share_price).toFixed(2);
-					
-					tradeTable.addRow(trade_portions[i], shares, share_price, cost);
-				}
-				tradeTable.close();
-				
-				
-				TradeSummary.updateOnChange();    
-
-		//	});
+				tradeTable.addRow(trade_portions[i], shares, share_price, cost);
+			}
+			tradeTable.close();
+			
+			
+			TradeSummary.updateOnChange();    
 			
 		}
 	});	
